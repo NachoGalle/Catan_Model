2139d76bf228b630fc8ecb9ba2c25800f4bd0f74
NachoGalle <nachogallego01@gmail.com>
2025-08-05 20:11:26 -0300

    Arreglo grafo posiciones, modifico clase posiciones y arreglo funcion cargar_tablero

--- generar_mapas.py
+++ generar_mapas.py
@@ -11,14 +11,14 @@ random.shuffle(terrenos)
 NUMEROS = (5,2,6,3,8,10,9,12,11,4,8,10,9,4,5,6,3,11)
 
 class posicion():
-    def __init__(self,loc:tuple =None,pob:bool=False,ciud: bool=False,vecinos: dict={},recursos: list=[],puerto: list = [0,0,0,0,0]):
+    def __init__(self,loc:tuple =None,pob:bool=False,ciud: bool=False,vecinos: dict= None ,recursos: list= None,puerto: list = None):
         self.loc=loc
         self.ocupable=True
         self.poblado= pob
         self.ciudad= ciud
-        self.vecinos= vecinos
-        self.recursos = recursos
-        self.puerto = puerto
+        self.vecinos= vecinos if vecinos is not None else {}
+        self.recursos = recursos if recursos is not None else []
+        self.puerto = puerto if puerto is not None else [0,0,0,0,0]
         self.atributos = {"loc": loc, "ocupable": self.ocupable, "poblado": pob, "ciudad": ciud , "vecinos" : self.vecinos}
 
     def add_recurso(self,recurso: tuple):
@@ -66,10 +66,15 @@ class tablero():
                 elif actual == 10 or actual == 11: actual = 18
                 elif actual == 1 or actual == 12: actual = 13
                 else: actual = 19
-    
+        self._pasar_a_tuplas()
+
     def generar_random(self):
         n=random.randint(1,12)
         self.generar_desde(n)
+
+    def _pasar_a_tuplas(self):
+        for i in range(1,20):
+            self.espacios[i]=tuple(self.espacios[i])
 
 def n_prob(n: int) -> float:
     return (6-abs(n-7))/36
@@ -92,9 +97,8 @@ generar_grafo_posiciones(G)
 
 def cargar_tablero(G: nx.Graph,Tablero: tablero):
     for i in [(2,0),(3,0),(4,0),(2,1),(3,1),(4,1)]:
-        nodo = G.nodes[i]["posicion"]
         recurso = Tablero.espacios[5]
-        nodo.add_recurso(recurso)
+        G.nodes[i]["posicion"].add_recurso(recurso)
 
 
 tab = tablero()
@@ -109,6 +113,8 @@ print(len(G.nodes[2,0]["posicion"].recursos))
 for i in [(2,0),(3,0),(4,0),(2,1),(3,1),(4,1)]:
     print(str(G.nodes[i]) + "recs" + str(G.nodes[i]["posicion"].recursos))
 
-nx.draw(G,pos, node_color="skyblue", with_labels=True)
+pos = nx.get_node_attributes(G, "pos")
+nx.draw(G, pos=pos, node_color="skyblue")
 plt.show()
+
 print(list(G.nodes(data="posicion")))
